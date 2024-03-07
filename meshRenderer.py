456805ad12a2fed6037d26301772c39ae91e22a9
ArchieUrquhart <128182020+ArchieUrquhart@users.noreply.github.com>
2024-03-07 12:03:58 +0000

    meshRenderer.py

--- meshRenderer.py
+++ meshRenderer.py
@@ -3,32 +3,12 @@ import numpy as np
 import math
 
 #SETUP
-#   import mesh from mesh file that contains vertex face and normal data
-#       file format:
-#         v point1
-#         v point2
-#         v point3
-#            ...
-#         f pointA pointB pointC
-#         f pointA pointB pointC
-#         f pointA pointB pointC
-#            ...
-#         n x y z
-#         n x y z
-#         n x y z
-#            ...
-#
-#         eg.
-#         v 1.0, 0.0, 1.0 (x, y, z position)
-#         v 3.0, 1.0, 1.0
-#            ...
-#         f 1, 2, 3 (each element is a corresponding vertex index)
-#            ...
-#         v 3, 1, 1
+#   import mesh from obj file 
+#   init points array
+#   init triangles array
 
-# POINTS ARE LABELED CLOCKWISE (when facing from exterior)
+#   init triangles
 
-#   initialize triangles
 #   define rotation matricies
 #   define projection matrix
 
@@ -37,13 +17,19 @@ import math
 #   rotate triangles
 #       numpy matmul with each point againt rot mat
 
-#   find normals if not included
+#   find normals
+#       find edge vectors
 #       numpy cross product
 
 #   project valid triangles
-#       numpy dot prod with vector to 
+#       numpy dot prod with vector to "camera"
 #       if dot prod < 0 then comense
 #       numpy matmul with each point againt proj mat
+
+#   shade triangles
+#       init light vector(s)
+#       Shade triangles depending on normal light dot prod
+
 
 #DRAW TRIANGLES
 
